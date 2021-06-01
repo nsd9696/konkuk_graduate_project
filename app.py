96c6fb7377cd3e8c955eeac7c043abd8668b8a22
nsd9696 <61867199+nsd9696@users.noreply.github.com>
2021-06-01 16:32:10 +0900

    commit 0601_4

--- app.py
+++ app.py
@@ -21,46 +21,6 @@ templates = Jinja2Templates(directory="templates")
 async def index(request:Request):
     return templates.TemplateResponse("index.html",{'request':request})
 
-@app.post('/compression')
-async def compress(request:Request, file:UploadFile = File(...), 
-size_mult: str=Form(...), detail_threshold: str=Form(...), max_depth:str=Form(...), target_depth:str=Form(...)):
-
-    """
-    타노스 엔진으로 이미지를 압축한다.
-    - size_mult: quad tree 알고리즘 전 이미지 resize parameter
-    - detail_threshold: 특정 분할되어 있는 영역의 RGB pixel 값들의 분포 오차의 합에 대한 임계값.
-        분포 오차의 합이 detail_threshold보다 클 경우 해당 영역을 한번 더 4분할 한다.
-        해당 파라미터가 낮을수록 더 세분화하여 압축한다(압축률이 떨어진다).
-    - max_depth: 최대로 이미지를 4분할 할 수 있는 횟수
-    - target_depth: 4분할 하고자 하는 횟수
-    """
-    # upload image
-    content = await file.read()
-    with open(f"static/raw_image/{file.filename}", "wb") as f:
-        f.write(content)
-    file_path = f"/static/raw_image/{file.filename}"
-
-    #compression
-    size_mult=float(size_mult)
-    detail_threshold=int(detail_threshold)
-    max_depth=int(max_depth)
-    target_depth=int(target_depth)
-    image = Image.open(BytesIO(content))
-    image = image.resize((int(image.size[0] * size_mult), int(image.size[1] * size_mult)))
-    quadtree = QuadTree(image,int(detail_threshold),int(max_depth))
-    image,temp = quadtree.create_image(int(target_depth),show_lines=False)
-    save_path = './static/thanosed_image'
-    isdir = os.path.isdir(save_path)
-
-    if isdir == True:
-        image.save(f"{save_path}/{file.filename}")
-    else:
-        os.makedirs(save_path)
-        image.save(f"{save_path}/{file.filename}")
-    compressed_path = f"/static/thanosed_image/{file.filename}"
-    return templates.TemplateResponse("index.html",{'request':request,'file_path':file_path, 'compressed_path':compressed_path,
-    'size_mult':size_mult,'detail_threshold':detail_threshold,'max_depth':max_depth,'target_depth':target_depth})
-
 @app.post("/predict")
 async def compress(request:Request, compressed_path: str=Form(...)):
     print(compressed_path)
